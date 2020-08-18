0c022db0670a6e50755b6b508b6fbe5e406b959b
Jan Radziminski <radziminski.j@gmail.com>
2020-08-18 12:17:09 +0200

    Added new icon library made for react

--- src/views/Skills.js
+++ src/views/Skills.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { DiCss3, DiJavascript1, DiReact, DiMongodb, DiFirebase } from 'react-icons/di';
-import SimpleIcons from 'simple-icons';
+import {
+    ReactJs,
+    Typescript,
+    Javascript,
+    Css3,
+    Mongodb,
+    Firebase,
+} from '@icons-pack/react-simple-icons';
 import SectionTitle from '../components/SectionTitle';
 import Subtitle from '../components/Subtitle';
 import ProgressBar from '../components/ProgressBar';
@@ -17,7 +24,9 @@ const Skills = ({ language }) => {
     const otherTech = mainSkills.map((el, i) => {
         if (el.type === 'web') return null;
         if (el.name.length > 15)
-            return <ProgressBar key={i} percentage={el.percentage} label={el.name} isTextLong={true} />;
+            return (
+                <ProgressBar key={i} percentage={el.percentage} label={el.name} isTextLong={true} />
+            );
         return <ProgressBar key={i} percentage={el.percentage} label={el.name} />;
     });
 
@@ -41,21 +50,20 @@ const Skills = ({ language }) => {
         <section className="section-skills section--light" name="section-skills">
             <Linker type="dark" />
             <div className={`section-skills__title ${titleCenteringClass}`}>
-                <SectionTitle type={titleType}>{getText(language).sectionTitles.skills}</SectionTitle>
+                <SectionTitle type={titleType}>
+                    {getText(language).sectionTitles.skills}
+                </SectionTitle>
             </div>
             <div className={parentContainerClass}>
                 <div className={firstChildContainerClass}>
                     <Subtitle>{getText(language).sectionTitles.skillsSubFirst}:</Subtitle>
                     <div className={skillsContainerClass}>
-                        <DiJavascript1 className="section-skills__icon" />
-                        <DiCss3 className="section-skills__icon" />
-                        <DiReact className="section-skills__icon" />
-                        <DiMongodb className="section-skills__icon" />
-                        <DiFirebase className="section-skills__icon" />
-                        <div
-                            className="section-skills__icon"
-                            dangerouslySetInnerHTML={{ __html: SimpleIcons.get('TypeScript').svg }}
-                        ></div>
+                        <Javascript className="section-skills__icon" size={48} />
+                        <Typescript className="section-skills__icon" size={48} />
+                        <Css3 className="section-skills__icon" size={48} />
+                        <ReactJs className="section-skills__icon" size={48} />
+                        <Mongodb className="section-skills__icon" size={48} />
+                        <Firebase className="section-skills__icon" size={48} />
                     </div>
                 </div>
                 <div className={midChildContainerClass}>
